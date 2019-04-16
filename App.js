2690138d6f36542055e6c06086a173354a3ac282
LUCAS SKAF BUNCANNA DE FREITAS <50001390899@admin.br>
2019-04-16 17:56:14 -0300

    App.Js V2

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 
  var CARD_CONTAINER = document.getElementsByClassName('card-container')[0];
- var NOMES = ["Anderson","Beatriz","Fabiana","Everton","teste"];
+ var NOMES = ["Anderson","Beatriz","Caio", "Daniela","Everton","Fabiana", "Gabriel", "Hortenca", "Igor", "Joana"];
  var cards = [];
- var ref=firebase.database().ref('card/');
+ var ref = firebase.database().ref('card');
 
 function criarcard(){
 
   var card = {
     nome: NOMES[Math.floor(Math.random()* NOMES.length-1)],
     idade: Math.floor(Math.random()* 22 + 18),
-    curtidas: 0
-
+    curtidas: 0,
+        
   };
-
   
 
-  ref.push(card)
-      .then(snapshot =>{ 
+  ref.push(card).then(snapshot =>{ 
       adicionarCardATela(card, snapshot.key);
 
     });
@@ -25,12 +23,14 @@ function criarcard(){
 };
 
 function deletar(id) {
-  console.log(id)
-  //ref.child(id).remove().then(()=>{
-    //var card = document.getElementById(id);
-   // card.remove();
-  //})  
+  
+
+  ref.child(id).remove().then(()=>{
+    
+  })  
+
 };
+
 
 function curtir(id){
 console.log(id)
@@ -42,38 +42,44 @@ console.log(id)
 
 document.addEventListener("DOMContentLoaded", function(){
   ref.once('value').then(snapshot=>{
-    console.log('child', snapshot.child('-LcDAqjLK9xRAHRHyRXr'))
+//    console.log('child', snapshot.child('-LcDAqjLK9xRAHRHyRXr'))
 
-    console.log('exists()', snapshot.exists())
+   // console.log('exists()', snaspshot.exists())
 
-    console.log('hasChild() comentarios', snapshot.hasChild('-LcDAqjLK9xRAHRHyRXr/comentarios'))
+  //  console.log('hasChild() comentarios', snapshot.hasChild('-LcDAqjLK9xRAHRHyRXr/comentarios'))
 
-    console.log('hasChildren', snapshot.child('-LcDAqjLK9xRAHRHyRXr').hasChildren())
+   // console.log('hasChildren', snapshot.child('-LcDAqjLK9xRAHRHyRXr').hasChildren())
 
-    console.log('numChildren()', snapshot.numChildren())
+  //  console.log('numChildren()', snapshot.numChildren())
 
     console.log("chave: ", snapshot.key)
     snapshot.forEach(value =>{
       console.log('chave. ', value.key)
       adicionarCardATela(value.val(), value.key);
     });
-  });
+  });s
 });
 
-function adicionarCardATela(informacao) {
-  console.log(informacao.id)
-  
-  
+function adicionarCardATela(informacao, snapKey) {
+  console.log(informacao)
+  //header
+
   let header = document.createElement("h2");
   header.innerText = informacao.nome;
   header.classList.add('card-title');
 
-  let content = document.createElement("p")
-  content.classList.add('Card-Text');
-  content.innerText = informacao.idade + "anos."
+  //Content
+
+  let content = document.createElement("p");
+  content.classList.add('card-text');
+  content.innerText = informacao.idade + " anos.";
+
+  //Botões do Card
 
   let inner = document.createElement("div");
   inner.classList.add('row');
+
+  //BtnAdicionar
 
   let button_add = document.createElement("button");
   button_add.classList.add('btn', 'btn-link', 'col-3');
@@ -81,9 +87,14 @@ function adicionarCardATela(informacao) {
   button_add.innerText = '+'; 
   inner.appendChild(button_add);
 
- let counter = document.createElement("span");
- counter.innerHTML = informacao.curtidas;
- counter.classList.add("col-3", "text-center")
+  //Contador de Curtidas
+
+   let counter = document.createElement("span");
+   counter.innerHTML = informacao.curtidas;
+   counter.classList.add("col-3", "text-center");
+   inner.appendChild(counter);
+
+  //btnSubtrair
 
   let button_sub= document.createElement("button");
   button_sub.classList.add('btn', 'btn-link', 'col-3');
@@ -91,11 +102,15 @@ function adicionarCardATela(informacao) {
   button_sub.innerText = '-'; 
   inner.appendChild(button_sub);
 
+  //BtnDeletar
+
   let button_del= document.createElement("button");
   button_del.classList.add('btn', 'btn-link', 'col-3');
-  button_del.setAttribute('onclick', "deletar("+informacao.id + ")");
-  button_del.innerText = 'x'; 
+  button_del.setAttribute('onclick', 'deletar("' + snapKey + '")');
+  button_del.innerText = 'X'; 
   inner.appendChild(button_del);
+
+  //Card
 
   let card = document.createElement("div");
   card.classList.add('card');
@@ -103,12 +118,15 @@ function adicionarCardATela(informacao) {
   let card_body = document.createElement("div");
   card_body.classList.add('card-body');
 
+  //popula card
+
   card_body.appendChild(header);
   card_body.appendChild(content);
   card_body.appendChild(inner);
+  card.appendChild(card_body)
   
-
-  CARD_CONTAINER.appendChild(card_body);
+  //insere no container
+  CARD_CONTAINER.appendChild(card);
 
 };
 
